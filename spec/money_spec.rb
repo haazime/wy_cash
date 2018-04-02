9ca7a52702f6d80ba9dbdbc136a2a5ce434a2e7f
hajime.sueyoshi <hajime.sueyoshi@classi.jp>
2018-04-02 15:50:21 +0900

    pos 928

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -4,18 +4,18 @@ describe '通貨' do
   describe '掛け算' do
     it do
       five = Money.dollar(5)
-      expect(five.times(2)).to eq(Dollar.new(10))
-      expect(five.times(3)).to eq(Dollar.new(15))
+      expect(five.times(2)).to eq(Money.dollar(10))
+      expect(five.times(3)).to eq(Money.dollar(15))
     end
   end
 
   describe '同値性' do
     it do
-      expect(Dollar.new(5)).to eq(Dollar.new(5))
-      expect(Dollar.new(5)).to_not eq(Dollar.new(6))
+      expect(Money.dollar(5)).to eq(Money.dollar(5))
+      expect(Money.dollar(5)).to_not eq(Money.dollar(6))
       expect(Franc.new(5)).to eq(Franc.new(5))
       expect(Franc.new(5)).to_not eq(Franc.new(6))
-      expect(Franc.new(5)).to_not eq(Dollar.new(5))
+      expect(Franc.new(5)).to_not eq(Money.dollar(5))
     end
   end
 
