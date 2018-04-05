0652e6722a2c7e88d6f6d47520742d12141d4423
hajime.sueyoshi <hajime.sueyoshi@classi.jp>
2018-04-05 14:17:51 +0900

    Dollar.equals()をMoneyに移動

--- typescript/src/money/Money.ts
+++ typescript/src/money/Money.ts
@@ -1,2 +1,12 @@
 export default class Money {
+  public amount: number;
+
+  constructor() {
+    this.amount = 0;
+  }
+
+  public equals(other: Object): boolean {
+    const money: Money = <Money> other;
+    return this.amount == money.amount;
+  }
 }
