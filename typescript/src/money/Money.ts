0bd8c2f5ceb14ab0cd6da2fd11ee2f519e73de18
hajime.sueyoshi <hajime.sueyoshi@classi.jp>
2018-04-05 15:07:21 +0900

    add Money.dollar

--- typescript/src/money/Money.ts
+++ typescript/src/money/Money.ts
@@ -1,5 +1,13 @@
-export default class Money {
+import Dollar from './Dollar';
+
+abstract class Money {
   public amount: number;
+
+  public abstract times(multiplier: number): Money;
+
+  public static dollar(amount: number): Dollar {
+    return new Dollar(amount);
+  }
 
   constructor() {
     this.amount = 0;
@@ -10,3 +18,4 @@ export default class Money {
     return this.amount == money.amount && this.constructor.toString() == money.constructor.toString();
   }
 }
+export default Money;
--- typescript/test/Money.spec.ts
+++ typescript/test/Money.spec.ts
@@ -4,19 +4,19 @@ import { Dollar, Franc, Money } from '../src/money/index';
 describe('Money', () => {
   describe('testMultiplication', () => {
     it('', () => {
-      const five: Dollar = new Dollar(5);
-      expect(five.times(2).equals(new Dollar(10))).to.be.true;
-      expect(five.times(3).equals(new Dollar(15))).to.be.true;
+      const five: Money = Money.dollar(5);
+      expect(five.times(2).equals(Money.dollar(10))).to.be.true;
+      expect(five.times(3).equals(Money.dollar(15))).to.be.true;
     })
   })
 
   describe('testEquality', () => {
     it('', () => {
-      expect(new Dollar(5).equals(new Dollar(5))).to.be.true;
-      expect(new Dollar(5).equals(new Dollar(6))).to.be.false;
+      expect(Money.dollar(5).equals(Money.dollar(5))).to.be.true;
+      expect(Money.dollar(5).equals(Money.dollar(6))).to.be.false;
       expect(new Franc(5).equals(new Franc(5))).to.be.true;
       expect(new Franc(5).equals(new Franc(6))).to.be.false;
-      expect(new Franc(5).equals(new Dollar(5))).to.be.false;
+      expect(new Franc(5).equals(Money.dollar(5))).to.be.false;
     })
   })
 
